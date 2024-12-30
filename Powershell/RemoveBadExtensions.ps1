5cb7b4da772924bfbaa4b5d8956e3126ead8c387
Clemv95 <32482023+Clemv95@users.noreply.github.com>
2024-12-30 09:52:03 +0100

    Create RemoveBadExtensions.ps1

---
+++ Powershell/RemoveBadExtensions.ps1
@@ -0,0 +1,28 @@
+
+# Define the path to the Chrome extensions folder for the found user
+$chromeExtensionsPath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Extensions"
+# List of extension IDs to remove
+$extensionsToRemove = @(
+    "dpggmcodlahmljkhlmpgpdcffdaoccni",
+    "nnpnnpemnckcfdebeekibpiijlicmpom",
+    "kkodiihpgodmdankclfibbiphjkfdenh",
+    "oaikpkmjciadfpddlpjjdapglcihgdle",
+    "acmfnomgphggonodopogfbmkneepfgnh",
+    "mnhffkhmpnefgklngfmlndmkimimbphc",
+    "befflofjcniongenjmbkgkoljhgliihe",
+    "cedgndijpacnfbdggppddacngjfdkaca",
+    "egmennebgadmncfjafcemlecimkepcle",
+    "cplhlgabfijoiabgkigdafklbhhdkahj",
+    "gadbpecoinogdkljjbjffmiijpebooce",
+    "oeiomhmbaapihbilkfkhmlajkeegnjhe"
+)
+# Remove the extensions
+foreach ($extensionId in $extensionsToRemove) {
+    $extensionPath = Join-Path -Path $chromeExtensionsPath -ChildPath $extensionId
+    if (Test-Path $extensionPath) {
+        Remove-Item -Path $extensionPath -Recurse -Force
+        Write-Host "Extension $extensionId removed."
+    } else {
+        Write-Host "Extension $extensionId not found."
+    }
+}
